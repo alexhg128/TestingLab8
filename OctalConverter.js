b0f95ecf654c1caa00e9504e0911f3958b2111bd
Alejandro Hahn <halexhg@gmail.com>
2020-04-15 13:02:44 -0600

    LAB8: Finished octal converter

--- OctalConverter.js
+++ OctalConverter.js
@@ -1,5 +1,21 @@
 function convert(decimal) {
-
+    var result = 0;
+    var result_array = [];
+    var temp = decimal;
+    while(true) {
+        var quotient = Math.floor(temp / 8);
+        var rest = temp - (quotient * 8);
+        temp = quotient;
+        console.log(`Temp / 8 = ${quotient} + ${rest}`);
+        result_array.push(rest);
+        if(quotient == 0) {
+            break;
+        }
+    }
+    for(var i = result_array.length - 1; i >= 0; i--) {
+        result += (result_array.pop() * (10 ** i));
+    }
+    return result;
 }
 
 module.exports.convert = convert;
